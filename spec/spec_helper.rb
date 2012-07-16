c7b6a32b8f413a4b701128ccc6bcc700195ed7b4
jkeiser <jkeiser@opscode.com>
2012-07-16 16:03:45 -0700

    Simple integration test that starts a client (verifies nothing)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,13 @@ WATCH = lambda { |x| puts x } unless defined?(WATCH)
 # Do not change the gsub.
 Dir["spec/support/**/*.rb"].map { |f| f.gsub(%r{.rb$}, '') }.each { |f| require f }
 
+class TestConfig
+  class << self
+    attr_accessor :service_url_base
+    attr_accessor :client_private_key_path
+  end
+end
+
 RSpec.configure do |config|
   config.mock_with :rspec
   config.filter_run :focus => true
@@ -25,4 +32,10 @@ RSpec.configure do |config|
 
   config.run_all_when_everything_filtered = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.formatter = 'documentation'
+
+  PushyClient::Log.level = :debug
+
+  TestConfig.service_url_base = "http://33.33.33.10:10003/organizations/pushy"
+  TestConfig.client_private_key_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'keys', 'client_private.pem'))
 end
