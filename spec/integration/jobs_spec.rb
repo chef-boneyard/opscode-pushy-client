695bf3f6f1fcadeb51ad6ca601216cb12b62f2cd
jkeiser <jkeiser@opscode.com>
2012-09-01 14:01:11 -0700

    Track new "unavailable" node state

--- spec/integration/jobs_spec.rb
+++ spec/integration/jobs_spec.rb
@@ -244,7 +244,7 @@ describe PushyClient::App do
         })
         # TODO check immediacy!  This could erroneously succeed on timing out.
         job = wait_for_job_status(response['uri'], 'quorum_failed')
-        job['nodes'].should == { 'nacked' => [ 'DONKEY' ] }
+        job['nodes'].should == { 'unavailable' => [ 'DONKEY' ] }
       end
     end
 
@@ -262,7 +262,7 @@ describe PushyClient::App do
         # TODO we should ensure that this happened due to down detection, not
         # timeout.  Fine for now, because there is no timeout :)
         job = wait_for_job_status(response['uri'], 'quorum_failed')
-        job['nodes'].should == { 'nacked' => [ 'DONKEY' ] }
+        job['nodes'].should == { 'unavailable' => [ 'DONKEY' ] }
       end
     end
   end
