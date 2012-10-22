9bfa550fc626d4208b8f2b4ec49ff67f831aa009
jamesc <james@opscode.com>
2012-10-22 16:47:19 -0700

    update to use authenticated calls to pushy API

--- lib/pushy-client/client/app.rb
+++ lib/pushy-client/client/app.rb
@@ -1,9 +1,29 @@
+#
+# Author:: James Casey (<james@opscode.com>)
+# Author:: Mark Anderson (<mark@opscode.com>)
+# Author:: John Keiser (<john@opscode.com>)
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'chef/rest'
 require 'time'
 require 'pp'
 
 module PushyClient
   class App
-    DEFAULT_SERVICE_URL_BASE = "localhost:10003/organization/clownco"
+    DEFAULT_SERVICE_URL_BASE = "https://localhost/organization/clownco"
 
     attr_accessor :service_url_base
     attr_accessor :client_private_key_path
@@ -20,6 +40,13 @@ module PushyClient
       PushyClient::Log.info "Using configuration endpoint: #{service_url_base}"
       PushyClient::Log.info "Using private key: #{client_private_key_path}"
       PushyClient::Log.info "Using node name: #{node_name}"
+    end
+
+    def get_rest(path, raw)
+      @rest_endpoint ||= Chef::REST.new(service_url_base || DEFAULT_SERVICE_URL_BASE,
+                                        node_name,
+                                        client_private_key_path)
+      @rest_endpoint.get_rest(path, raw)
     end
 
     def start
