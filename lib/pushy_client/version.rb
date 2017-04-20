cb764d261e5e6e1ae8d3c46e314c9d2e0e650a06
Lamont Granquist <lamont@scriptkiddie.org>
2017-04-20 11:47:40 -0700

    bump minor version for chef-13
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/pushy_client/version.rb
+++ lib/pushy_client/version.rb
@@ -1,4 +1,4 @@
-# @copyright Copyright 2014 Chef Software, Inc. All Rights Reserved.
+# @copyright Copyright 2014-2017, Chef Software Inc. All Rights Reserved.
 #
 # This file is provided to you under the Apache License,
 # Version 2.0 (the "License"); you may not use this file
@@ -18,6 +18,6 @@
 # Note: the version must also be updated in
 # omnibus/config/projects/push-jobs-client.rb
 class PushyClient
-  VERSION = "2.2.0"
+  VERSION = "2.3.0"
   PROTOCOL_VERSION = "2.0"
 end
