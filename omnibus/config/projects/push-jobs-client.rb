8ec8e4f6481f6ea6462f8b5f7aae949216eb6ced
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2017-05-10 14:47:59 -0400

    Update push-jobs to compile libzmq on windows.
    See chef/omnibus-software#830 for details.
    
    Also updates .kitchen.yml to build windows-2012r2 because we don't use
    win 2008 builders in the CI anymore.
    
    Signed-off-by: Kartik Null Cating-Subramanian <ksubramanian@chef.io>

--- omnibus/config/projects/push-jobs-client.rb
+++ omnibus/config/projects/push-jobs-client.rb
@@ -43,13 +43,18 @@ override :chef,           version: "v13.0.118"
 override :ohai,           version: "v13.0.1"
 
 # Need modern bundler if we wish to support x-plat Gemfile.lock.
-# Otherwise win32-api pins for windows will break non-windows builds for bundler < 1.14.
+# Unfortunately, 1.14.x series has issues with BUNDLER_VERSION variables exported by
+# the omnibus cookbook. Bump to it after the builders no longer set that environment
+# variable.
 override :bundler,        version: "1.13.7"
 override :rubygems,       version: "2.6.12"
 override :ruby,           version: "2.4.1"
 
-# Share pins with ChefDK
-override :libzmq,         version: "4.0.5"
+# Default in omnibus-software was too old.  Feel free to move this ahead as necessary.
+override :libsodium,      version: "1.0.12"
+# Pick last version in 4.0.x that we have tested on windows.
+# Feel free to bump this if you're willing to test out a newer version.
+override :libzmq,         version: "4.0.7"
 
 ######
 
