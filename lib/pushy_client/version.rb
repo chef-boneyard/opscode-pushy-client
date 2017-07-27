3e922b60080ec32c83d6688903c6d0ff21644ca7
Chef Expeditor <eng-services-ops+chef-ci@chef.io>
2017-07-27 16:51:24 +0000

    Bump version to 2.3.5 by Chef Expeditor

--- lib/pushy_client/version.rb
+++ lib/pushy_client/version.rb
@@ -18,6 +18,6 @@
 # Note: the version must also be updated in
 # omnibus/config/projects/push-jobs-client.rb
 class PushyClient
-  VERSION = "2.3.4"
+  VERSION = "2.3.5"
   PROTOCOL_VERSION = "2.0"
 end
